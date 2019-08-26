2fba17914d0ad07ada278a3e4a5799e668e47eda
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-26 05:59:15 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,13 +1,15 @@
-require 'pry'
-
 #This is the groceries hash we'll be passing in to the get_the_values method
+# def get_the_values(groceries)
 # groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
+#   dairy: ["milk", "yogurt", "cheese"],
+#   vegetable: ["carrots", "broccoli", "cucumbers"],
+#   meat: ["chicken", "steak", "salmon"],
+#   grains: ["rice", "pasta"]
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
+  groceries.values
+  groceries.values.flatten
+  get_the_values(groceries)
 end
