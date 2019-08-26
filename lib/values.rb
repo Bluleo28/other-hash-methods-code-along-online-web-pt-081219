d19fcf940fd1e22ae1f19f26836c566a26da2c7b
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-26 06:06:32 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,5 +1,4 @@
-#This is the groceries hash we'll be passing in to the get_the_values method
-# def get_the_values(groceries)
+#get_the_values returns all the values from the hash get_the_values method
 # groceries = {
 #   dairy: ["milk", "yogurt", "cheese"],
 #   vegetable: ["carrots", "broccoli", "cucumbers"],
